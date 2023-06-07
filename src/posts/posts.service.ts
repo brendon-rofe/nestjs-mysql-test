dad071ffea49c3effb092a9fe3f7c143db0ed280
Bravo1B9 <brofe93@gmail.com>
2023-06-07 10:01:02 +0000

    Posts service setup

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(@InjectRepository(Post) private postRepo: Repository<Post>) {};
+
+  async findAll(): Promise<Post[]> {
+    return await this.postRepo.find();
+  }
+
+  async create(post: Post): Promise<Post> {
+    const newPost = this.postRepo.create(post);
+    await this.postRepo.save(newPost);
+    return newPost;
+  }
+};
