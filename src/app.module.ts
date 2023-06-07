f267f40f7b0e5636eb9e14ac7991723991e45ed4
Bravo1B9 <brofe93@gmail.com>
2023-06-07 09:45:43 +0000

    Establish db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "root",
+      "password": "example",
+      "database": "nestjs",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+    })
+  ],
 })
 export class AppModule {}
